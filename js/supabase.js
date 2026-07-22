ac7fe0f2b991a2fb6c97d948b4d43cc6a7f853f4
overexposure-blog <overexposure@qq.com>
2026-07-22 14:50:28 +0800

    Site updated: 2026-07-22 14:50:25

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,5 @@
 const supabaseClient =
 supabase.createClient(
-    "https://bzpahicignjpcxdxhvmv.supabase.co",
-    "sb_publishable_89CMyvAFoIwh71LeUQbNEw_TQ9s1yfb"
+    https://bzpahicignjpcxdxhvmv.supabase.co,
+    sb_publishable_89CMyvAFoIwh71LeUQbNEw_TQ9s1yfb
+)
