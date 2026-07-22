9cff7322167f7e6e8b1604825c2a5fe9e21f21b8
overexposure-blog <overexposure@qq.com>
2026-07-22 14:53:50 +0800

    Site updated: 2026-07-22 14:53:47

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,15 @@
+
+const SUPABASE_URL = 
+"https://bzpahicignjpcxdxhvmv.supabase.co";
+
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_89CMyvAFoIwh71LeUQbNEw_TQ9s1yfb";
+
+
 const supabaseClient =
 supabase.createClient(
-    "https://bzpahicignjpcxdxhvmv.supabase.co",
-    "sb_publishable_89CMyvAFoIwh71LeUQbNEw_TQ9s1yfb"
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 );
+
